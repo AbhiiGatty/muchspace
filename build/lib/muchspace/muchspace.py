05ebf8e1bffe476ed8fb2db4230093a6ba6253a5
Abhiigatty <abhiigatty@gmail.com>
2018-06-12 10:21:05 +0530

    grabinfo --> getinfo and Built 0.3.2

--- build/lib/muchspace/muchspace.py
+++ build/lib/muchspace/muchspace.py
@@ -9,16 +9,16 @@ class MuchSpace:
         Email: abhiigatty@gmail.com\n
             muchspace: The link status checking CLI.
             Example usage:
-            muchspace grabinfo <FILE_PATH> or
-            muchspace grabinfo <FILE_PATH> --report # The --report will generate a json report 
+            muchspace getinfo <FILE_PATH> or
+            muchspace getinfo <FILE_PATH> --report # The --report will generate a json report 
             i.e
-            muchspace grabinfo --file-path <FILE_PATH> [--report]"""
+            muchspace getinfo --file-path <FILE_PATH> [--report]"""
 
     def __init__(self):
         print("muchspace v2 - Pre-Alpha")
 
     """ Function to stack the given directory based on extensions"""
-    def grabinfo(self, file_path, report = False):
+    def getinfo(self, file_path, report = False):
         """ muchspace: The link status checking CLI.""" 
         fastRequests.main(file_path=file_path, report=report) # Call main to start processing file_path
 
